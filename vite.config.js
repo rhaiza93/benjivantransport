e1162421c66069c2fb9a11dca0638b0d7be20663
Lounes Nait Atmane <louneshg@gmail.com>
2025-11-05 00:12:46 +0100

    fix: clean install vite build

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// Configuration de Vite
 export default defineConfig({
   plugins: [react()],
-  base: './',
-  build: {
-    outDir: 'dist',
-  },
 })
